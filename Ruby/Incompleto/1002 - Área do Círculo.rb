de89dd0debb4f107879f1c251b36e9ed313bbdd0
Felipe Prestes Aranalde <felipe.aranalde@gmail.com>
2018-03-13 00:33:56 -0300

    em analise

---
+++ Ruby/Incompleto/1002 - Área do Círculo.rb
@@ -0,0 +1,5 @@
+R=gets().to_f()
+C = 3.14159 * R * R;
+A = C.to_f()
+puts "A="+A
+puts "\n"
---
+++ Ruby/Incompleto/1008 - Salário.rb
@@ -0,0 +1,8 @@
+	NUMBER=gets.to_i()
+	THORA=gets.to_i()
+	PHORA=gets.to_f()
+	SALARY = THORA * PHORA
+	printf "NUMBER = U$ %d",NUMBER
+	puts "\n"
+	printf "SALARY = U$ %.2f",SALARY
+	puts "\n"
